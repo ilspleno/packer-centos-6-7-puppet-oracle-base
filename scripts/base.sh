9961f081b4b29ee9b64381138d8257a83a8d81a5
Brian Woodruff <brian@thewoodruffs.org>
2015-10-06 13:46:32 -0400

    Learned a lot about how kickstart scripts work. Previous ks.cfg wasn't set up to actually update packages.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-whoami > /tmp/whoami
 
 ## Very temporary
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+#echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+# Add EPEL and Puppet repos
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+
+# Install dev tools and misc packages needed later on
+yum -y install curl rsync
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
