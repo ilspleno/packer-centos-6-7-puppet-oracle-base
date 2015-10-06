c1d8db83433188ee8dd55be9058940d5e8f9b6d3
Brian Woodruff <brian@thewoodruffs.org>
2015-10-06 11:03:53 -0400

    Moved packages back into kickstart to see if virtualbox will actually build...

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,12 +2,7 @@
 whoami > /tmp/whoami
 
 ## Very temporary
-#echo "nameserver 8.8.8.8" > /etc/resolv.conf
-
-# Update the box
-#yum -y update
-yum -y groupinstall "Development Tools"
-yum -y install wget kernel-devel kernel-headers vim wget curl rsync
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
