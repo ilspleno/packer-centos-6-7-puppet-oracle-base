8e561c4e0379e28880b12c15931e904096dabc1d
Brian Woodruff <brian@thewoodruffs.org>
2015-10-27 14:28:07 -0400

    Adding script to clean up network adadpters

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -8,14 +8,14 @@ fi
 # Install the VMWare Tools from a linux ISO.
 
 #wget http://192.168.0.185/linux.iso -P /tmp
-mkdir -p /mnt/vmware
-mount -o loop /home/vagrant/linux.iso /mnt/vmware
+sudo mkdir -p /mnt/vmware
+sudo mount -o loop /home/vagrant/linux.iso /mnt/vmware
 
 cd /tmp
-tar xzf /mnt/vmware/VMwareTools-*.tar.gz
+sudo tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 
-umount /mnt/vmware
-rm -fr /home/vagrant/linux.iso
+sudo umount /mnt/vmware
+sudo rm -fr /home/vagrant/linux.iso
 
-/tmp/vmware-tools-distrib/vmware-install.pl -d
-rm -fr /tmp/vmware-tools-distrib
+sudo /tmp/vmware-tools-distrib/vmware-install.pl -d
+sudo rm -fr /tmp/vmware-tools-distrib
